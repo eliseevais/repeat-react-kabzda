1661e0da25303abfde7c4d921423a27f9088878c
Irina Eliseeva <irina.eliseeva1991@gmail.com>
2025-03-09 19:49:53 +0300

    sprint 01 week 01: add Rating, Accordion, types

---
+++ src/components/Accordion/Accordion.tsx
@@ -0,0 +1,42 @@
+type AccordionPropsType = {
+  title: string
+  collapsed: boolean
+}
+
+export const Accordion = (props: AccordionPropsType) => {
+  if (props.collapsed) {
+    return (
+      <div>
+        <AccordionTitle title={props.title} />
+      </div>
+    )
+  }
+  if (!props.collapsed) {
+    return (
+      <div>
+        <AccordionTitle title={props.title} />
+        <AccordionBody/>
+      </div>
+    )
+  }
+}
+
+type AccordionTitlePropsType = {
+  title: string
+}
+
+const AccordionTitle = (props: AccordionTitlePropsType) => {
+  return (
+    <h3>{props.title}</h3>
+  )
+}
+
+const AccordionBody = () => {
+  return (
+    <ul>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+    </ul>
+  )
+}
